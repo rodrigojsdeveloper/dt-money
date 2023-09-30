cc01a26fe05c917a2ea30965ce960f19367a652c
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-09-30 15:24:05 -0300

    feat: add Button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,59 @@
+interface IButtonProps {
+  isSearch?: boolean;
+  text: string;
+  type?: "button" | "submit" | "reset";
+  maxWidth?: "max-w-147" | "max-w-152";
+  height?: "h-54";
+  background?: "bg-grey-2";
+  color?: "text-color-primary-1";
+}
+
+const Button = ({
+  isSearch,
+  text,
+  type,
+  maxWidth,
+  height,
+  background,
+  color,
+}: IButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`w-full ${maxWidth ? maxWidth : "max-w-0"} ${
+        height ? height : "h-50"
+      } ${
+        background ? background : "bg-color-primary-1"
+      } border border-solid border-color-primary-1 font-bold text-sm ${
+        color ? color : "text-white"
+      } rounded-def`}
+    >
+      {isSearch ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="20"
+          height="20"
+          viewBox="0 0 20 20"
+          fill="none"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M9.0625 3.4375C5.9559 3.4375 3.4375 5.9559 3.4375 9.0625C3.4375 12.1691 5.9559 14.6875 9.0625 14.6875C12.1691 14.6875 14.6875 12.1691 14.6875 9.0625C14.6875 5.9559 12.1691 3.4375 9.0625 3.4375ZM1.5625 9.0625C1.5625 4.92036 4.92036 1.5625 9.0625 1.5625C13.2046 1.5625 16.5625 4.92036 16.5625 9.0625C16.5625 13.2046 13.2046 16.5625 9.0625 16.5625C4.92036 16.5625 1.5625 13.2046 1.5625 9.0625Z"
+            fill="#00B37E"
+          />
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M13.0402 13.0402C13.4063 12.6741 13.9999 12.6741 14.366 13.0402L18.1629 16.8371C18.529 17.2032 18.529 17.7968 18.1629 18.1629C17.7968 18.529 17.2032 18.529 16.8371 18.1629L13.0402 14.366C12.6741 13.9999 12.6741 13.4063 13.0402 13.0402Z"
+            fill="#00B37E"
+          />
+        </svg>
+      ) : null}
+
+      {text}
+    </button>
+  );
+};
+
+export { Button };
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,44 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "color-primary": {
+          "1": "#00875F",
+          "2": "#015F43",
+        },
+        grey: {
+          "1": "#121214",
+          "2": "#202024",
+          "3": "#29292E",
+          "4": "#323238",
+          "5": "#7C7C8A",
+          "6": "#8D8D99",
+          "7": "#C4C4CC",
+          "8": "#E1E1E6",
+        },
+        white: "#FFFFFF",
+        red: "#F75A68",
+      },
+      maxWidth: {
+        "147": "147px",
+        "152": "152px",
+      },
+      height: {
+        "50": "50px",
+        "54": "54px",
+      },
+      borderRadius: {
+        "def": "4px",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;
